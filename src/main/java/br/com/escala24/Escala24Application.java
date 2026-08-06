d0e41f2023f50412067a1c5285b6987bfb63a817
João Vinicius Esquerdo de Andrade <joaovinicius0130@gmail.com>
2026-08-06 20:38:27 -0300

    chore: initialize Spring Boot project

---
+++ src/main/java/br/com/escala24/Escala24Application.java
@@ -0,0 +1,13 @@
+package br.com.escala24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Escala24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Escala24Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/escala24/Escala24ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.escala24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Escala24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

abef839a02df4bf3dcb1d8c2487d0dcf8f533ca8
João Vinicius Esquerdo de Andrade <joaovinicius0130@gmail.com>
2026-08-09 14:44:28 -0300

    feat: add firefighter management service

--- src/main/java/br/com/escala24/repository/FirefighterRepository.java
+++ src/main/java/br/com/escala24/repository/FirefighterRepository.java
@@ -2,11 +2,19 @@ package br.com.escala24.repository;
 
 import java.util.Optional;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.EntityGraph;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.escala24.entity.Firefighter;
 
 public interface FirefighterRepository extends JpaRepository<Firefighter, Long> {
+
+    @Override
+    @EntityGraph(attributePaths = "user")
+    List<Firefighter> findAll();
 
     Optional<Firefighter> findByRegistration(String registration);
 
